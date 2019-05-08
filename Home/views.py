6c66b652c082fe928967ce86e2c1f3eb397b6e87
Xin Pan <pan_xin1991@live.cn>
2019-05-08 21:43:35 +0800

    modify-2019-5-6

--- Home/views.py
+++ Home/views.py
@@ -4,6 +4,6 @@ from django.shortcuts import HttpResponse
 
 def Home(request):
     uContext = {}
-    uContext['nav'] = [{'left':[{'id':'Home','endpoint':'Home','text':'Home',},{'id':'Home','endpoint':'Home','text':'Home','submenu':[{'id':'Home1','endpoint':'Home','text':'Home'},{'id':'Home2','endpoint':'Home','text':'Home'}]}]}]
-    uContext['sidebar']=[{'dd':11}]
+    uContext['nav'] = [{'left':[{'id':'Home','endpoint':'Home','text':'Home',},{'id':'Home','endpoint':'Home','text':'Home','submenu':[{'id':'Home1','endpoint':'Home','text':'Home'},{'id':'Home2','endpoint':'Home','text':'Home'}]}],'right':[{'id':'Home','endpoint':'Home','text':'Home',},{'id':'Home','endpoint':'Home','text':'Home','submenu':[{'id':'Home1','endpoint':'Home','text':'Home'},{'id':'Home2','endpoint':'Home','text':'Home'}]}]}]
+    uContext['sidebar']=[{'id':'Home','endpoint':'Home','text':'Home',},{'id':'Home','endpoint':'Home','text':'Home','submenu':[{'id':'Home1','endpoint':'Home','text':'Home'}]}]
     return render(request,'index.html',uContext)
