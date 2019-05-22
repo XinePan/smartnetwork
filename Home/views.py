cde0e417f076600cd88c5c8752c5fe77f5897d25
XinePan <pan_xin1991@live.cn>
2019-05-22 23:03:30 +0800

    modify

--- Home/views.py
+++ Home/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render
+from smartnetwork import user_settings
 from django.shortcuts import HttpResponse
 # Create your views here.
 
 def Home(request):
-    uContext = {}
-    uContext['nav'] = [{'left':[{'id':'Home','endpoint':'Home','text':'Home'},{'id':'Home','endpoint':'Home','text':'Home','submenu':[{'id':'Home1','endpoint':'Home','text':'Home'},{'id':'Home2','endpoint':'Home','text':'Home'}]}],'right':[{'id':'Home','endpoint':'Home','text':'Home',},{'id':'Home','endpoint':'Home','text':'Home','submenu':[{'id':'Home1','endpoint':'Home','text':'Home'},{'id':'Home2','endpoint':'Home','text':'Home'}]}]}]
-    uContext['sidebar']=[{'id':'Home','endpoint':'Home','text':'Home',},{'id':'Home','endpoint':'Home','text':'Home','submenu':[{'id':'Home1','endpoint':'Home','text':'Home'}]}]
-    uContext['section_nav'] = [{'id': 'Home', 'endpoint': 'Home', 'text': 'Home1', 'type':3},
-                           {'id': 'Home', 'endpoint': 'Home', 'text': 'Home',
-                            'submenu': [{'id': 'Home1', 'endpoint': 'Home', 'text': 'Home'}]}]
+    uContext = {'nav':user_settings.NAV,'sidebar':user_settings.SECTION_NAV,'section_nav':user_settings.SECTION_NAV}
+
+
     return render(request,'index.html',uContext)
---
+++ smartnetwork/user_settings.py
@@ -0,0 +1,30 @@
+"""
+Django user settings for smartnetwork project.
+
+"""
+
+import os
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+#nav
+NAV = {
+    'left':[
+        {'id':'Home','endpoint':'Home','text':'Home','type':1}
+    ],
+     'right':[
+         {'id':'Home','endpoint':'Home','text':'游客','type':4,
+          'submenu':[
+             {'id':'Home1','endpoint':'Home','text':'关注','type':4},
+             {'id':'Home2','endpoint':'Home','text':'注册','type':4}
+         ]},
+     ],
+}
+
+#sidebar
+SIDEBAR = [{'id':'Home','endpoint':'Home','text':'Home','type':3},
+           ]
+#section_nav
+SECTION_NAV = [{'id': 'Home', 'endpoint': 'Home', 'text': 'Home1', 'type':3},
+               ]
